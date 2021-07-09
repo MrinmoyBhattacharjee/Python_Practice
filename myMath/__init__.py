3a302998b4574d6391afc6f83ecb813858d1a957
MrinmoyBhattacharjee <65397082+MrinmoyBhattacharjee@users.noreply.github.com>
2021-07-10 00:37:58 +0530

    Add files via upload

---
+++ myMath/__init__.py
@@ -0,0 +1,7 @@
+# outer __init__.py
+from add import addition
+from divide import division
+from multiply import multiply
+from subtract import subtract
+from Adv.fib import fibonacci
+from Adv.squareroot import squareroot
