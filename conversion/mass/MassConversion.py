7463884041f9990e1f746cb265ee2bdef5266e23
MrinmoyBhattacharjee <65397082+MrinmoyBhattacharjee@users.noreply.github.com>
2021-07-10 00:35:22 +0530

    Add files via upload

---
+++ conversion/mass/MassConversion.py
@@ -0,0 +1,18 @@
+#MassConversion.py
+""" Mass conversion function between Kilogram,Tonne and Pound """
+#functions
+
+def kgtotonne(x):
+    """Returns x converted to tonne"""
+    return x*0.001
+def tonnetokg(x):
+    """Returns x converted to kilogram"""
+    return x/0.001
+
+def kgtopound(x):
+    """Returns x converted to pound"""
+    return x*2.20462
+
+def poundtokg(x):
+    """Returns x converted to kilogram"""
+    return x/2.20462
